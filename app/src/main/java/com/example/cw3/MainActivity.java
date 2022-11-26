715288fecedcdeb5faafb8113867c5408b6d312f
äbubakar20-02 <abubakarx72@gmail.com>
2022-11-26 22:41:41 +0000

    main page connects to run and statistic page.

--- app/src/main/java/com/example/cw3/MainActivity.java
+++ app/src/main/java/com/example/cw3/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.cw3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+
+import com.example.cw3.databinding.ActivityMainBinding;
+
+// Running logo resource taken from: https://www.iconsdb.com/black-icons/running-icon.html
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Links the main activity with it's viewmodel and creates binding object
+        activityMainBinding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(activityMainBinding.getRoot());
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int height = displayMetrics.heightPixels;
-        int width = displayMetrics.widthPixels;
+        //When the start button is pressed go to it's activity
+        activityMainBinding.StartButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Course.class);
+            startActivity(intent);
+        });
+
+        //When the start button is pressed go to it's activity
+        activityMainBinding.StatisticButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, StatisticActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/cw3/StatisticActivity.java
@@ -0,0 +1,14 @@
+package com.example.cw3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
+    }
+}
